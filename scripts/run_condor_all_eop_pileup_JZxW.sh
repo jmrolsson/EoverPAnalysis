445b635e0965551e05f0c2d86fd025664334da52
jolsson <j.mr.olsson@gmail.com>
2016-07-14 09:54:55 -0500

    Updated filelists

--- scripts/run_condor_all_eop_pileup_JZxW.sh
+++ scripts/run_condor_all_eop_pileup_JZxW.sh
@@ -11,15 +11,13 @@ if [ $# -eq 0 ]
     tag=$1
     today=$(date +"%Y%m%d")
     files_JZ0W=EoverP/filelists/mc15_13TeV_pileup_JZ0W_test200k.txt
-    files_JZ1W=EoverP/filelists/mc15_13TeV_pileup_JZ1W_test200k.txt
+    files_JZ1W=EoverP/filelists/mc15_13TeV_pileup_JZ1W_all.txt
 
     mkdir -p results
 
     echo "---> Running JZxW pileup MC samples:"
     echo xAH_run.py --files ${files_JZ0W} --inputList --config EoverP/scripts/config_eop_mc.py --submitDir results/condor_test_eop_pileup_JZ0W_${today}_${tag} --verbose --force condor --optFilesPerWorker 10
-    # xAH_run.py --files ${files_JZ0W} --inputList --config EoverP/scripts/config_eop_mc.py --submitDir results/condor_test_eop_pileup_JZ0W_${today}_${tag} --verbose --force condor --optFilesPerWorker 10
     echo xAH_run.py --files ${files_JZ1W} --inputList --config EoverP/scripts/config_eop_mc.py --submitDir results/condor_test_eop_pileup_JZ1W_${today}_${tag} --verbose --force condor --optFilesPerWorker 10
-    # xAH_run.py --files ${files_JZ1W} --inputList --config EoverP/scripts/config_eop_mc.py --submitDir results/condor_test_eop_pileup_JZ1W_${today}_${tag} --verbose --force condor --optFilesPerWorker 10
 
     echo "---> Write to logfile:"
     echo "# ---> "$(date +"%Y-%m-%d:%H:%M:%S") >> results/run_condor_eop_pileup.log
